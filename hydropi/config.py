33eec38037928cf7f545df1f643028096697b918
ncentola <ncentola@iloan.com>
2018-05-16 16:22:44 -0500

    cast to ints, add pins

--- hydropi/config.py
+++ hydropi/config.py
@@ -7,11 +7,18 @@ class Config():
         cfp.read(path_to_config_file)
 
         self.method = cfp.get('general', 'method')
-        self.day_start = cfp.get('general', 'day_start')
-        self.day_end = cfp.get('general', 'day_end')
+        self.day_start = int(cfp.get('general', 'day_start'))
+        self.day_end = int(cfp.get('general', 'day_end'))
+
+        self.air_pump_pin = int(cfp.get('general', 'air_pump_pin'))
+        self.lights_pin = int(cfp.get('general', 'lights_pin'))
+        self.water_pump_pin = int(cfp.get('general', 'water_pump_pin'))
+
+        self.lights_starts = list(range(self.day_start, self.day_end+1))
+        self.lights_on_mins = 60
 
         self.air_pump_starts = json.loads(cfp.get(self.method, 'air_pump_starts'))
-        self.air_pump_time_on_mins = cfp.get(self.method, 'air_pump_time_on_mins')
+        self.air_pump_time_on_mins = int(cfp.get(self.method, 'air_pump_time_on_mins'))
 
         self.water_pump_starts = json.loads(cfp.get(self.method, 'water_pump_starts'))
-        self.water_pump_time_on_mins = cfp.get(self.method, 'water_pump_time_on_mins')
+        self.water_pump_time_on_mins = int(cfp.get(self.method, 'water_pump_time_on_mins'))
