ee4c4a6fea06ef615636332757b269547a803492
ncentola <ncentola@iloan.com>
2018-05-16 16:23:13 -0500

    init methods done, switch method done

--- hydropi/hardware.py
+++ hydropi/hardware.py
@@ -1,37 +1,68 @@
-import RPi.GPIO as GPIO
+from .schedule import Schedule
+from warnings import warn
+
+# use this for dev where you can't install the gpio lib
+gpio_working=False
+try:
+    import RPi.GPIO as GPIO
+    gpio_working=True
+except:
+    warn('RPi.GPIO not found, GPIO functionality will not work')
+
 
 class Hardware():
-    
-    def __init__(self, pin):
+
+    def __init__(self, pin, raw_schedule):
         self.is_on = False
         self.pin = pin
-        self.schedule = create_schedule()
+        self.schedule = Schedule(raw_schedule)
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin, GPIO.OUT)
+        if gpio_working:
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(pin, GPIO.OUT)
 
 
     def switch(self, current_minute):
-        if current_minute in self.schedule:
+        # check schedule to see if the thing is supposed to be on in the current minute
+        if self.schedule.minutes[current_minute]:
             if self.is_on:
                 pass
             else:
-                GPIO.output(self.pin, 1)
+                if gpio_working:
+                    GPIO.output(self.pin, 1)
+                self.is_on = True
         else:
             if self.is_on:
-                GPIO.output(self.pin, 0)
+                if gpio_working:
+                    GPIO.output(self.pin, 0)
+                self.is_on = False
 
 
 class Lights(Hardware):
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        pin = config.lights_pin
+        raw_schedule = {
+            'hour_starts':      config.lights_starts,
+            'duration_mins':    config.lights_on_mins
+        }
+        super(Lights, self).__init__(pin=pin, raw_schedule=raw_schedule)
 
 
 class AirPump(Hardware):
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        pin = config.air_pump_pin
+        raw_schedule = {
+            'hour_starts':      config.air_pump_starts,
+            'duration_mins':    config.air_pump_time_on_mins
+        }
+        super(AirPump, self).__init__(pin, raw_schedule)
 
 
 class WaterPump(Hardware):
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        pin = config.water_pump_pin
+        raw_schedule = {
+            'hour_starts':      config.water_pump_starts,
+            'duration_mins':    config.water_pump_time_on_mins
+        }
+        super(WaterPump, self).__init__(pin, raw_schedule)
